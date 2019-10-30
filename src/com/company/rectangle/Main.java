daf624b555a533bfc1682d7fb5539d6fbdcf8063
dudq <dudq.xmt@gmail.com>
2019-10-30 14:53:10 +0700

    add QuadraticEquation.class file

--- src/com/company/rectangle/Main.java
+++ src/com/company/rectangle/Main.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.rectangle;
 
 import java.util.Scanner;
 
--- src/com/company/rectangle/Rectangle.java
+++ src/com/company/rectangle/Rectangle.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.company.rectangle;
 
 public class Rectangle {
     double width;
